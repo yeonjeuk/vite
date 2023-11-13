4c33d2db816318ff4b6685aaa9f88e2137b8fe2e
yeonjeuk <duswpdnr9@gmail.com>
2023-11-13 17:17:40 +0900

    sub_page upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox
 workbox.setConfig({
   debug: !0
 });
-workbox.precaching.precacheAndRoute([{"revision":null,"url":"assets/index-d40d6541.js"},{"revision":null,"url":"assets/index-f217be3d.css"},{"revision":null,"url":"assets/webfontloader-523643f5.js"},{"revision":"6136cfca036af037b75b279d50e9c306","url":"index.html"},{"revision":"ab3cedaeaab6f9325c98e7abe76fb8c6","url":"registerSW.js"},{"revision":"951eaa5dcc734487158e7c747729576a","url":"manifest.webmanifest"}]);
+workbox.precaching.precacheAndRoute([{"revision":null,"url":"assets/index-94e33040.css"},{"revision":null,"url":"assets/index-b650c5c5.js"},{"revision":null,"url":"assets/webfontloader-523643f5.js"},{"revision":"e97ca1312e9e6e7eb50a498e72c92ce9","url":"index.html"},{"revision":"ab3cedaeaab6f9325c98e7abe76fb8c6","url":"registerSW.js"},{"revision":"951eaa5dcc734487158e7c747729576a","url":"manifest.webmanifest"}]);
 workbox.routing.registerRoute(
   new RegExp("https://firebasestorage.googleapis.com/v0/b/pwa-camera.appspot.com/.*"),
   new workbox.strategies.StaleWhileRevalidate({

df55081a5be2ab8a720d1db703543fd96ca40c60
yeonjeuk <duswpdnr9@gmail.com>
2023-11-16 16:19:33 +0900

    스크롤 탑 고정

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox
 workbox.setConfig({
   debug: !0
 });
-workbox.precaching.precacheAndRoute([{"revision":null,"url":"assets/index-94e33040.css"},{"revision":null,"url":"assets/index-d4e8742e.js"},{"revision":null,"url":"assets/webfontloader-523643f5.js"},{"revision":"c4420b44b6cda4b6b5e6959c2f041df3","url":"index.html"},{"revision":"ab3cedaeaab6f9325c98e7abe76fb8c6","url":"registerSW.js"},{"revision":"951eaa5dcc734487158e7c747729576a","url":"manifest.webmanifest"}]);
+workbox.precaching.precacheAndRoute([{"revision":null,"url":"assets/index-94e33040.css"},{"revision":null,"url":"assets/index-990a59e1.js"},{"revision":null,"url":"assets/webfontloader-523643f5.js"},{"revision":"8c2d851912ffc3fefac83ea9ffaea279","url":"index.html"},{"revision":"ab3cedaeaab6f9325c98e7abe76fb8c6","url":"registerSW.js"},{"revision":"951eaa5dcc734487158e7c747729576a","url":"manifest.webmanifest"}]);
 workbox.routing.registerRoute(
   new RegExp("https://firebasestorage.googleapis.com/v0/b/pwa-camera.appspot.com/.*"),
   new workbox.strategies.StaleWhileRevalidate({
